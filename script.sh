017479f4cc3da2889ad56c91206084bc00cda8a4
ludovicm67 <ludovicmuller1@gmail.com>
2018-05-10 15:33:48 +0200

    kill nodeMembers at the end

--- script.sh
+++ script.sh
@@ -8,13 +8,27 @@ sleep 5
 node node.js localhost 1235 localhost 1234&
 NODE2=$!
 
-node nodeMember.js localhost 1242 localhost 1234
-node nodeMember.js localhost 1243 localhost 1234
-node nodeMember.js localhost 1244 localhost 1234
-node nodeMember.js localhost 1245 localhost 1234
-node nodeMember.js localhost 1246 localhost 1234
+node nodeMember.js localhost 1241 localhost 1234&
+NODEMEMBER1=$!
+node nodeMember.js localhost 1242 localhost 1234&
+NODEMEMBER2=$!
+node nodeMember.js localhost 1243 localhost 1234&
+NODEMEMBER3=$!
+node nodeMember.js localhost 1244 localhost 1234&
+NODEMEMBER4=$!
+node nodeMember.js localhost 1245 localhost 1234&
+NODEMEMBER5=$!
+node nodeMember.js localhost 1246 localhost 1234&
+NODEMEMBER6=$!
 
 sleep 20
 
 kill $NODE1
 kill $NODE2
+
+kill $NODEMEMBER1
+kill $NODEMEMBER2
+kill $NODEMEMBER3
+kill $NODEMEMBER4
+kill $NODEMEMBER5
+kill $NODEMEMBER6
